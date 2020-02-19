5f6b896ed78fe8341fae8446e693a245fa6ca092
takahashi <hanjiro2001@gmail.com>
2020-02-19 08:30:55 +0000

    views all

---
+++ resources/views/admin/news/edit.blade.php
@@ -0,0 +1,19 @@
+<!--以下課題部分 
+【応用】 3. の答えを実際に作成してみましょう。
+また、作成したbladeファイルにhtmlで記述して装飾してみましょう。
+-->
+
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>MyNews</title>
+    </head>
+    <body>
+        <h1>Myニュース作成画面</h1>
+    </body>
+</html>
