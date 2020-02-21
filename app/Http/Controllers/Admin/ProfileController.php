b0655afc1e48687943e17d6636fe7008df26f807
takahashi <hanjiro2001@gmail.com>
2020-02-21 11:28:07 +0000

    php 14 assignment

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+
+use App\Profile;
 
 class ProfileController extends Controller
 {
@@ -13,9 +14,21 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
     
-    public function create()
+    public function create(Request $request)
     {
+        $this->validate($request, News::$rules);
+
+        $profile = new Profile;
+        $form = $request->all();
+        
+        unset($form['_token']);
+
+        $profile->fill($form);
+        $profile->save();
+
         return redirect('admin/profile/create');
+        
+        
     }
     
     public function edit()
